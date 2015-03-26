fe063d8bc7f9cd4ab5e13a1e231c8f2b2148c23a
rhnorment <norment@gmail.com>
2015-03-26 17:50:17 -0500

    added active admin with admin user

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,3 @@
-%w(pages errors devise activ_admin).each do |controller|
+%w(pages errors devise active_admin).each do |controller|
   Rails.application.config.assets.precompile += ["#{controller}.css", "#{controller}.js"]
 end
