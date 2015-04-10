5a6d1d788f681739fcfecb449e17f0d344c44656
rhnorment <norment@gmail.com>
2015-04-10 11:19:36 -0500

    removed devise and active admin functionality.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,3 @@
-%w(pages errors devise active_admin).each do |controller|
+%w(pages errors).each do |controller|
   Rails.application.config.assets.precompile += ["#{controller}.css", "#{controller}.js"]
 end
